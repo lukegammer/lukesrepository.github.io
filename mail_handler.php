fed21f4b5cab8cbe72233a8f47bcf3776529f891
Luke Gammer <lukegammer1@gmail.com>
2019-01-08 13:08:46 +0000

    First upload

---
+++ mail_handler.php
@@ -0,0 +1,21 @@
+﻿<?php
+if(isset($_POST['submit'])){
+ $Fname=$_POST['Fname'];
+ $lName=$_POST['lName'];
+ $number=$_POST['number'];
+ $email=$_POST['email'];
+ $website=$_POST['website'];
+ $msg=$_POST['msg'];
+ 
+ $to='lukegammer@uk2.net';
+ $subject='Frome Submission';
+ $message="FirstName: ".$Fname."\n". "LastName: ".$lName."\n"."Phone: ".$number."\n". "website: ".$website."\n". "Wrote the following: "."\n\n".$msg;
+ $headers="From: ".$email;
+ 
+ if(mail($to, $subject, $message, $headers)){
+  echo "<h1>Send Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+ }else{
+  echo "Something went wrong!";
+ }
+ 
+}
